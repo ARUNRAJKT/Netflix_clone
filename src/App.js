4eec348ea56f11c244f040a2d84e83fe9e1aa938
Arun Raj K T <arunraj44799@gmail.com>
2024-10-30 17:34:16 +0530

    Initial commit of Netflix clone project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import { ROUTES } from './constants/routes';
+import Home from './pages/home';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
+import Browse from './pages/browse';
 
 function App() {
+  // Assuming `user` comes from your context or Redux store
+  const user = null; // Replace with actual user state
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path={ROUTES.SIGN_IN}
+          element={
+            user ? <Navigate to={ROUTES.BROWSE} /> : <Signin />
+          }
+        />
+
+        <Route
+          path={ROUTES.SIGN_UP}
+          element={
+            user ? <Navigate to={ROUTES.BROWSE} /> : <Signup />
+          }
+        />
+
+        <Route
+          path={ROUTES.BROWSE}
+          element={
+            user ? <Browse /> : <Navigate to={ROUTES.SIGN_IN} />
+          }
+        />
+
+        <Route
+          path={ROUTES.HOME}
+          element={
+            user ? <Navigate to={ROUTES.BROWSE} /> : <Home />
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
